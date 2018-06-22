173b67e418aeb1c836f3759036f1fcc151887887
niclaflamme <nic@laflamme.io>
2018-06-22 12:42:21 -0400

    wip

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -39,14 +39,8 @@ alias v='vim'
 alias vi='vim'
 
 
-# UNITO
-alias unito='cd ~/unito/code'
-alias conn='unito; and cd connectors'
-alias cons='unito; and cd console'
-alias ucomm='unito; and cd ucommon'
-alias sw='unito; and cd sync-worker'
-alias unitosymlinks='unito; and ./internal-tools/dev/local_libs.sh'
-alias unitotime='unito; and cd ../timesheets; and ll'
+# INVIVO
+alias invivo='cd ~/invivo/code'
 
 
 # SOUND
