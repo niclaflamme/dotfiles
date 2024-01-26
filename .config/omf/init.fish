3a1da916ef7efee466cf4b29bdaaccd2cdb7b02d
Nic Laflamme <nic@laflamme.io>
2024-01-26 13:45:36 -0500

    yolo

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -62,13 +62,54 @@ alias brsd='bun run start:dev'
 # ONWARD
 alias ow='cd ~/onward/monorepo'
 alias ow:mono='cd ~/onward/monorepo'
+alias ow:ui='cd ~/onward/monorepo/packages/ui'
+alias ow:app='cd ~/onward/monorepo/apps/app'
+alias ow:api='cd ~/onward/monorepo/apps/app'
 
 alias ow:pscale='ow:mono; and pscale connect onward-db --port 3309'
 alias ow:temporal='ow:mono; and temporal server start-dev'
 alias ow:hookdeck='ow:mono; and hookdeck listen 8910'
-alias ow:dev='ow:mono; and yarn dev:app'
+alias ow:api:dev='ow:mono; and cd ./services/api; and yarn dev'
+alias ow:app:dev='ow:mono; and cd ./apps/app; and yarn dev'
 
 
 # PATH COMPLETION
 set -gx PATH ~/.cargo/bin $PATH
 set -gx PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
+
+
+
+## >> GRAPHITE FISH COMPLETION ~ start
+
+# git helpers adapted from fish git completion
+function __fish_git_local_branches
+    command git for-each-ref --format='%(refname:strip=2)' refs/heads/ 2>/dev/null
+end
+
+function __fish_git_remote_branches
+    command git for-each-ref --format="%(refname:strip=3)" refs/remotes/ 2>/dev/null
+end
+
+# graphite helpers
+function __gt_command_completions
+    set -lx SHELL (type -p fish)
+    set -l command (commandline -opc)
+    # uncomment to include options, e.g. -q, --help
+    # $command --get-yargs-completions
+    # uncomment to exclude options (default)
+    $command --get-yargs-completions | string replace -r '\-.*' ''
+end
+
+# disable file completions for the entire command
+complete -c gt -f
+
+# add completions as provided by CLI
+complete -c gt -a "(__gt_command_completions)"
+
+# commands that take branches
+complete -c gt -x -n "__fish_seen_subcommand_from checkout co bco delete dl rename rn onto --onto -o tr utr track untrack" -a "(__fish_git_local_branches)"
+
+# gt downstack get takes remote branches
+complete -c gt -x -n "__fish_seen_subcommand_from ds dsg get g" -n "__fish_seen_subcommand_from get dsg" -a "(__fish_git_remote_branches)"
+
+## << GRAPHITE FISH COMPLETION ~ end
