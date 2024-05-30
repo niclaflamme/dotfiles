45eafdef63ec38079f5a3ada63021b8e57ec653e
Nic Laflamme <nic@laflamme.io>
2024-05-30 11:03:50 -0400

    yolo

--- .config/omf/init.fish
+++ .config/omf/init.fish
@@ -76,7 +76,7 @@ set -gx PATH /Applications/Postgres.app/Contents/Versions/latest/bin $PATH
 
 
 # ENV
-set -Ux GIT_EDITOR zed
+set -Ux GIT_EDITOR "zed --wait"
 
 
 ## >> GRAPHITE FISH COMPLETION ~ start
@@ -112,4 +112,4 @@ complete -c gt -x -n "__fish_seen_subcommand_from checkout co bco delete dl rena
 # gt downstack get takes remote branches
 complete -c gt -x -n "__fish_seen_subcommand_from ds dsg get g" -n "__fish_seen_subcommand_from get dsg" -a "(__fish_git_remote_branches)"
 
-## << GRAPHITE FISH COMPLETION ~ ending
+## << GRAPHITE FISH COMPLETION ~ ended
